13effef811803f4d4f2a132644f6490b2e45c221
anand91m <anand91m@gmail.com>
2024-01-25 20:43:56 +0100

    Fix relative path issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myPortfolio/'
+  base: '/myPortfolio/',
   plugins: [react()],
   server: {
     port: 8001,
